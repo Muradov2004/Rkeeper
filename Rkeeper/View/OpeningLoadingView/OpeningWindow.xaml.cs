8928ac428c3316bd8e8c043b673c8a129516a54a
Ali <112820884+Muradov2004@users.noreply.github.com>
2023-04-05 05:04:29 +0400

    Add project files.

---
+++ Rkeeper/View/OpeningLoadingView/OpeningWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Rkeeper.View.LoginRegisterView;
+using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Rkeeper.View.OpeningLoadingView
+{
+	/// <summary>
+	/// Interaction logic for OpeningWindow.xaml
+	/// </summary>
+	public partial class OpeningWindow : Window
+	{
+
+		public OpeningWindow()
+		{
+
+			InitializeComponent();
+
+            System.Timers.Timer timer = new()
+            {
+                Interval = 3000
+            };
+
+            timer.Elapsed += (sender, args) =>
+            {
+
+                timer.Stop();
+                
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    LoginRegisterWindow LoginWindow = new();
+                    LoginWindow.Show();
+
+                    Close();
+                });
+            };
+
+            timer.Start();
+
+        }
+
+	}
+}

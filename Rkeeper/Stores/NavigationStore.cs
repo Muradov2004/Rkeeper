232de21cda7913f85951bbd27f7d859aa7c9a088
Ali <112820884+Muradov2004@users.noreply.github.com>
2023-04-06 17:10:20 +0400

    Novigation store and base viewmodel addded

---
+++ Rkeeper/Stores/NavigationStore.cs
@@ -0,0 +1,23 @@
+﻿using Rkeeper.ViewModel;
+using System;
+
+namespace Rkeeper.Stores;
+
+public class NavigationStore
+{
+
+    public event Action? CurrentViewModelChanged;
+
+    private BaseVM? _currentViewModel;
+
+    public BaseVM? CurrentViewModel
+    {
+        get { return _currentViewModel; }
+        set
+        {
+            _currentViewModel = value;
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+
+}
---
+++ Rkeeper/ViewModel/BaseVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Rkeeper.ViewModel;
+
+public abstract class BaseVM : INotifyPropertyChanged
+{
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void NotifyPropertyChanged([CallerMemberName] string? propertyName = null)
+    => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+}

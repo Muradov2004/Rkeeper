8c669273fb64661e97ea504255b95608fbbe2fc5
Ali <112820884+Muradov2004@users.noreply.github.com>
2023-04-08 00:25:57 +0400

    Main VM updated

--- Rkeeper/ViewModel/MainVM.cs
+++ Rkeeper/ViewModel/MainVM.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Rkeeper.Stores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rkeeper.ViewModel
+namespace Rkeeper.ViewModel;
+
+public class MainVM : BaseVM
 {
-    class MainVM
+
+    private readonly NavigationStore _navigationStore;
+    public BaseVM? CurrentVM => _navigationStore.CurrentVM;
+
+    public MainVM(NavigationStore navigationStore)
     {
+        _navigationStore = navigationStore;
+        navigationStore.CurrentVMChanged += NavigationStore_CurrentVMChanged;
     }
+
+    private void NavigationStore_CurrentVMChanged()
+    => NotifyPropertyChanged(nameof(CurrentVM));
+
 }

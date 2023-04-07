7b96008cb05a5bac512a47ed8d1ff7ff6d547198
Ali <112820884+Muradov2004@users.noreply.github.com>
2023-04-08 00:55:30 +0400

    Model updated

--- Rkeeper/Model/Food.cs
+++ Rkeeper/Model/Food.cs
@@ -8,8 +8,19 @@ namespace Rkeeper.Model;
 
 class Food
 {
+
     public string? Name { get; set; }
+
     public string? ImageSource { get; set; }
-    public int Price { get; set; }
+
+    public double Price { get; set; }
+
     public int Count { get; set; }
+
+    public Food(string? name, double price, string imageSource = "../Assets/MenuImages/DefaultMenuFoodImage.jpg")
+    {
+        Name = name;
+        ImageSource = imageSource;
+        Price = price;
+    }
 }
--- Rkeeper/Model/FoodMenu.cs
+++ Rkeeper/Model/FoodMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,4 +9,16 @@ namespace Rkeeper.Model;
 
 class FoodMenu
 {
+
+    public ObservableCollection<Food> Foods { get; set; }
+
+    public FoodMenu()
+    {
+        Foods = new ObservableCollection<Food>()
+        {
+            new("Food1",1.0),
+            new("Food2",1.0),
+            new("Food3",1.0)
+        };
+    }
 }
